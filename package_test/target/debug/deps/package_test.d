8c7fe44aeb9eb1e99ba1d346430d0d2f094bc5aa
Roberto Plinio Parreira Junior <robertoppjr@gmail.com>
2024-08-21 21:19:19 -0300

    Remove garbage folders

--- package_test/target/debug/deps/package_test.d
+++
@@ -1,5 +0,0 @@
-D:\RPLINIO WORKS\Projetos\_Study\Rust_Course_Dmitri\package_test\target\debug\deps\package_test.exe: src/main.rs
-
-D:\RPLINIO WORKS\Projetos\_Study\Rust_Course_Dmitri\package_test\target\debug\deps\package_test.d: src/main.rs
-
-src/main.rs:
--- package_test/target/debug/package_test.d
+++
@@ -1,1 +0,0 @@
-D:\RPLINIO\ WORKS\Projetos\_Study\Rust_Course_Dmitri\package_test\target\debug\package_test.exe: D:\RPLINIO\ WORKS\Projetos\_Study\Rust_Course_Dmitri\package_test\src\main.rs
